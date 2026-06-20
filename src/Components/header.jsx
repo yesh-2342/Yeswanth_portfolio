1227a32eff2d3bba24d7f5f7681109d5e9041a98
Yeswanth Kumar <bitraguntayeswanth2005@gmail.com>
2026-06-20 22:36:11 +0530

    i was chaged the header section becase there is no responsive page for mobile

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,12 +1,14 @@
 import './Header.css';
-function Header(){
-    return(
-        
 
-
-        <div className="header-container">
-    <h1 className="header">hi iam yeswanth kumar aspiring to be a full stack developer to know more about me visit my portfolio</h1>
-      </div>
-    )
+function Header() {
+    return (
+        <header className="header-container">
+            <h1 className="header">
+                Hi, I am Yeswanth Kumar, an aspiring full-stack developer.
+                Explore my portfolio to learn more about me and my work.
+            </h1>
+        </header>
+    );
 }
+
 export default Header;
